7f16d69367b4cc8563a8b5691d8a9ce1f5c8bc5b
letustryandlearn <letustryandlearn@gmail.com>
2022-07-18 22:54:47 +0530

    react_quarkus

---
+++ src/test/java/com/letustryandlearn/GreetingResourceIT.java
@@ -0,0 +1,9 @@
+package com.letustryandlearn;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+
+public class GreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
